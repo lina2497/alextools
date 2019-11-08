d7e08aefabe0f866aa3a562df7365a391c88d964
lina2497 <alex.betts@evobio.eu>
2019-11-08 09:22:02 +0000

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,50 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+se <- compiler::cmpfun(function(x) sqrt(var(x,na.rm = T)/length(x)))
+
+weighted_se <- function(x, w, na.rm = TRUE) sqrt(weighted_var(x, w, na.rm = TRUE)/length(x))
+
+weighted_var <- function(x, w = NULL, na.rm = FALSE) {
+  if (na.rm) {
+    na <- is.na(x) | is.na(w)
+    x <- x[!na]
+    w <- w[!na]
+  }
+
+  sum(w * (x - weighted.mean(x, w)) ^ 2) / (sum(w) - 1)
+}
+
+
+not_in <- function(x, y) {
+  !('%in%'(x, y))
+}
+
+`%not in%` <- function (x, table) {
+  is.na(match(x, table, nomatch = NA_integer_))
+}
+
+library(readxl)
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
